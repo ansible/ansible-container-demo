ffa2cdeef4d4b794c58260a18a771a09e10dfa6a
Chris Houseknecht <chousekn@redhat.com>
2016-12-13 16:34:07 -0500

    Remove the demo bits

--- project/authentication/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-
-class IsAccountOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view, account):
-        if request.user:
-            return account == request.user
-        return False

eb293e05d998818400334b315846ce75cee731ef
Chris Houseknecht <chousekn@redhat.com>
2018-01-22 22:26:30 -0500

    Fix urls

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from rest_framework_nested import routers
 from project.views import ApiV1RootView, IndexView
 from project.authentication.views import AccountViewSet, LoginView, LogoutView
